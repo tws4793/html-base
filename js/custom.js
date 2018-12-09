fb5e665c7dadb90a882efe7aaba3889ed667b05e
Teo Wei Shen <tws4793@gmail.com>
2018-12-09 17:24:38 +0800

    Add more templates and helpers for workshop use

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+var number = 1
+var name = "Your name"
+var gender = "male"
+
+for (var i = 0; i < 10; i++) {
+    // Repeat 10 times
+}
+
+if (gender == "male") {
+    alert("Male");
+} else {
+    alert("Female");
+}
+
+function c2f(temp){
+    return temp * (9 / 5) + 32;
+}
